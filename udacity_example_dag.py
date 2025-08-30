b118723dfc27f92cb6bf4d604d112a266c4309f4
AnuragSinghal16 <101972336+AnuragSinghal16@users.noreply.github.com>
2025-08-30 19:26:36 +0530

    Create udacity_example_dag.py

---
+++ udacity_example_dag.py
@@ -0,0 +1,130 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators import (StageToRedshiftOperator,
+                                LoadFactOperator,
+                                LoadDimensionOperator,
+                                DataQualityOperator)
+from helpers import SqlQueries  # provided helper class with SQL queries
+
+# ---------------------
+# Default DAG arguments
+# ---------------------
+default_args = {
+    'owner': 'udacity',
+    'depends_on_past': False,
+    'start_date': datetime(2019, 1, 12),
+    'retries': 3,
+    'retry_delay': timedelta(minutes=5),
+    'email_on_retry': False
+}
+
+dag = DAG('udac_example_dag',
+          default_args=default_args,
+          description='Load and transform data in Redshift with Airflow',
+          schedule_interval='@hourly',
+          catchup=False
+          )
+
+# ---------------------
+# Tasks
+# ---------------------
+
+start_operator = DummyOperator(task_id='Begin_execution', dag=dag)
+
+stage_events_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_events',
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_events",
+    s3_bucket="udacity-dend",
+    s3_key="log_data/{execution_date.year}/{execution_date.month}/{ds}-events.json",
+    json_path="s3://udacity-dend/log_json_path.json",
+    dag=dag
+)
+
+stage_songs_to_redshift = StageToRedshiftOperator(
+    task_id='Stage_songs',
+    redshift_conn_id="redshift",
+    aws_credentials_id="aws_credentials",
+    table="staging_songs",
+    s3_bucket="udacity-dend",
+    s3_key="song_data",
+    json_path="auto",
+    dag=dag
+)
+
+load_songplays_table = LoadFactOperator(
+    task_id='Load_songplays_fact_table',
+    redshift_conn_id="redshift",
+    table="songplays",
+    sql_query=SqlQueries.songplay_table_insert,
+    dag=dag
+)
+
+load_user_dimension_table = LoadDimensionOperator(
+    task_id='Load_user_dim_table',
+    redshift_conn_id="redshift",
+    table="users",
+    sql_query=SqlQueries.user_table_insert,
+    append_only=False,  # truncate-insert
+    dag=dag
+)
+
+load_song_dimension_table = LoadDimensionOperator(
+    task_id='Load_song_dim_table',
+    redshift_conn_id="redshift",
+    table="songs",
+    sql_query=SqlQueries.song_table_insert,
+    append_only=False,
+    dag=dag
+)
+
+load_artist_dimension_table = LoadDimensionOperator(
+    task_id='Load_artist_dim_table',
+    redshift_conn_id="redshift",
+    table="artists",
+    sql_query=SqlQueries.artist_table_insert,
+    append_only=False,
+    dag=dag
+)
+
+load_time_dimension_table = LoadDimensionOperator(
+    task_id='Load_time_dim_table',
+    redshift_conn_id="redshift",
+    table="time",
+    sql_query=SqlQueries.time_table_insert,
+    append_only=False,
+    dag=dag
+)
+
+run_quality_checks = DataQualityOperator(
+    task_id='Run_data_quality_checks',
+    redshift_conn_id="redshift",
+    tests=[
+        {'check_sql': "SELECT COUNT(*) FROM users WHERE userid IS NULL", 'expected_result': 0},
+        {'check_sql': "SELECT COUNT(*) FROM songs WHERE songid IS NULL", 'expected_result': 0}
+    ],
+    dag=dag
+)
+
+end_operator = DummyOperator(task_id='Stop_execution', dag=dag)
+
+# ---------------------
+# Dependencies
+# ---------------------
+start_operator >> [stage_events_to_redshift, stage_songs_to_redshift]
+
+[stage_events_to_redshift, stage_songs_to_redshift] >> load_songplays_table
+
+load_songplays_table >> [load_song_dimension_table,
+                         load_user_dimension_table,
+                         load_artist_dimension_table,
+                         load_time_dimension_table]
+
+[load_song_dimension_table,
+ load_user_dimension_table,
+ load_artist_dimension_table,
+ load_time_dimension_table] >> run_quality_checks
+
+run_quality_checks >> end_operator
